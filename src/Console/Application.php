ff1cabbc1bb483155ea108eabfb7d82e746a47cf
smori <shinichiro.mori.1983@gmail.com>
2018-03-22 12:49:46 +0900

    add PageCaptureCommand

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -6,6 +6,22 @@ use Symfony\Component\Console\Application as ConsoleApplication;
 
 class Application extends ConsoleApplication
 {
+    protected $configDirectory = null;
+
+    public function setConfigDirectory($path)
+    {
+        $this->configDirectory = $path;
+    }
+
+    public function getConfigPath($relativePath)
+    {
+        return sprintf(
+            '%s/%s',
+            rtrim($this->configDirectory, '/'),
+            ltrim($relativePath, '/')
+        );
+    }
+
     /**
      * {@inheritdoc}
      */
--- src/Console/Command/PageCaptureCommand.php
+++ src/Console/Command/PageCaptureCommand.php
@@ -1,10 +1,17 @@
 <?php
 namespace Momo\Selenium\Console\Command;
 
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\DriverCommand;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverDimension;
+use Facebook\WebDriver\WebDriverKeys;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class PageCaptureCommand extends Command
 {
@@ -22,5 +29,36 @@ class PageCaptureCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $seleniumConf = Yaml::parse(file_get_contents(
+            $this->getApplication()->getConfigPath('config_selenium.yml')
+        ));
+
+        $urlList = Yaml::parse(file_get_contents(
+            $this->getApplication()->getConfigPath('config_capture_list.yml')
+        ));
+
+        $webDriver = RemoteWebDriver::create(
+            $seleniumConf['url'],
+            DesiredCapabilities::chrome()
+        );
+
+        $webDriver
+            ->manage()
+            ->window()
+            ->setSize(new WebDriverDimension(
+                $seleniumConf['browser']['width'],
+                $seleniumConf['browser']['height']
+            ));
+
+        foreach ($urlList['list'] as $item) {
+            $webDriver->get($item['url']);
+            $webDriver->takeScreenshot(sprintf(
+                '%s/name.png',
+                $seleniumConf['screenshot_save_directory'],
+                $item['name']
+            ));
+        }
+
+        $webDriver->close();
     }
 }
