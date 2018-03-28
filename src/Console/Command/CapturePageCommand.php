7495311b99713a63be66ed55940f7c374146e3fa
smori <shinichiro.mori.1983@gmail.com>
2018-03-28 17:05:36 +0900

    add WebDriverConfiguration for validation of webdriver.yml

--- src/Console/Command/CapturePageCommand.php
+++ src/Console/Command/CapturePageCommand.php
@@ -6,6 +6,9 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverDimension;
 use Momo\Selenium\Browser\BrowserResolver;
 use Momo\Selenium\CaptureUtil\CaptureListFactory;
+use Momo\Selenium\Console\Config\WebDriverConfiguration;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,6 +17,11 @@ use Symfony\Component\Yaml\Yaml;
 
 class CapturePageCommand extends Command
 {
+    /**
+     * @var Symfony\Component\Config\Definition\Processor
+     */
+    protected $processor = null;
+
     protected $captureListFactory = null;
 
     protected $browserResolver = null;
@@ -30,38 +38,50 @@ class CapturePageCommand extends Command
 
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
+        $this->processor = new Processor();
         $this->captureListFactory = new CaptureListFactory();
         $this->browserResolver = new BrowserResolver();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $seleniumConf = Yaml::parse(file_get_contents(
+        $yml = Yaml::parse(file_get_contents(
             $this->getApplication()->getConfigPath('webdriver.yml')
         ));
 
+        try {
+            $webDriverConf = $this->processor->processConfiguration(
+                new WebDriverConfiguration(),
+                [$yml]
+            );
+        } catch (InvalidConfigurationException $e) {
+            $output->writeln('<error>Error in webdriver.yml</error>');
+
+            return 1;
+        }
+
         $captureList = $this->captureListFactory->create($input->getArgument('captureList'));
 
-        $browser = $this->browserResolver->resolve($seleniumConf['browser']['type']);
+        $browser = $this->browserResolver->resolve($webDriverConf['browser']['type']);
 
         $webDriver = RemoteWebDriver::create(
-            $seleniumConf['url'],
+            $webDriverConf['url'],
             $browser->getCapabilities(),
-            $seleniumConf['connection_timeout_ms'],
-            $seleniumConf['request_timeout_ms']
+            $webDriverConf['connection_timeout_ms'],
+            $webDriverConf['request_timeout_ms']
         );
 
         $webDriver
             ->manage()
             ->timeouts()
-            ->pageLoadTimeout($seleniumConf['page_load_timeout']);
+            ->pageLoadTimeout($webDriverConf['page_load_timeout']);
 
         $webDriver
             ->manage()
             ->window()
             ->setSize(new WebDriverDimension(
-                $seleniumConf['browser']['width'],
-                $seleniumConf['browser']['height']
+                $webDriverConf['browser']['width'],
+                $webDriverConf['browser']['height']
             ));
 
         try {
@@ -70,7 +90,7 @@ class CapturePageCommand extends Command
 
                 $imagePath = sprintf(
                     '%s/%s.png',
-                    $seleniumConf['screenshot_save_directory'],
+                    $webDriverConf['screenshot_save_directory'],
                     $item->getName()
                 );
 
---
+++ src/Console/Config/WebDriverConfiguration.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Momo\Selenium\Console\Config;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class WebDriverConfiguration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+
+        $rootNode = $treeBuilder->root('webdriver');
+        $rootNode
+            ->children()
+                ->scalarNode('url')
+                    ->isRequired()
+                ->end()
+                ->integerNode('connection_timeout_ms')
+                    ->defaultNull()
+                ->end()
+                ->integerNode('request_timeout_ms')
+                    ->defaultNull()
+                ->end()
+                ->arrayNode('browser')
+                    ->children()
+                        ->scalarNode('type')
+                            ->isRequired()
+                        ->end()
+                        ->integerNode('width')
+                            ->isRequired()
+                            ->min(1)
+                        ->end()
+                        ->integerNode('height')
+                            ->isRequired()
+                            ->min(1)
+                        ->end()
+                    ->end()
+                ->end()
+                ->integerNode('page_load_timeout')
+                    ->defaultNull()
+                ->end()
+                ->scalarNode('screenshot_save_directory')
+                    ->isRequired()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
