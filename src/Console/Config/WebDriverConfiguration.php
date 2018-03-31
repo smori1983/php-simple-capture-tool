1cd91187b1f20e6d3363f5e7b11f17b81012ecd4
smori <shinichiro.mori.1983@gmail.com>
2018-03-31 12:37:53 +0900

    fix CS

--- src/Console/Config/WebDriverConfiguration.php
+++ src/Console/Config/WebDriverConfiguration.php
@@ -2,8 +2,8 @@
 
 namespace Momo\Selenium\Console\Config;
 
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class WebDriverConfiguration implements ConfigurationInterface
 {
