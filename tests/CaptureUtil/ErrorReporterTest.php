e483645f867d3a8ce66dc60fb77c9c31ce7fd325
smori <shinichiro.mori.1983@gmail.com>
2019-02-02 03:04:34 +0900

    Refactor ErrorReporterTest

--- tests/CaptureUtil/ErrorReporterTest.php
+++ tests/CaptureUtil/ErrorReporterTest.php
@@ -25,7 +25,7 @@ class ErrorReporterTest extends \PHPUnit_Framework_TestCase
 
     public function testOneError()
     {
-        $this->SUT->add(new ErrorItem(new CaptureItem('site01', 'http://site01.example.com/'), new \Exception()));
+        $this->addError('site01', 'http://site01.example.com/', new \Exception());
 
         $this->SUT->report($this->output);
 
@@ -39,8 +39,8 @@ EOL;
 
     public function testTwoErrors()
     {
-        $this->SUT->add(new ErrorItem(new CaptureItem('site01', 'http://site01.example.com/'), new \Exception()));
-        $this->SUT->add(new ErrorItem(new CaptureItem('site02', 'http://site02.example.com/'), new \Exception()));
+        $this->addError('site01', 'http://site01.example.com/', new \Exception());
+        $this->addError('site02', 'http://site02.example.com/', new \Exception());
 
         $this->SUT->report($this->output);
 
@@ -51,5 +51,17 @@ site02, http://site02.example.com/, Exception
 EOL;
 
         $this->assertSame(trim($message), trim($this->output->fetch()));
+    }
+
+    /**
+     * Test DSL.
+     *
+     * @param string     $name
+     * @param string     $url
+     * @param \Exception $exception
+     */
+    private function addError($name, $url, \Exception $exception)
+    {
+        $this->SUT->add(new ErrorItem(new CaptureItem($name, $url), $exception));
     }
 }
